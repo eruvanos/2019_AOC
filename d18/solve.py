8130c41e83b28254d8c1f0a758f8bea47ad2a45e
Maic Siemering <maic@siemering.tech>
2020-01-04 22:20:34 +0100

    wip day 18

--- d18/solve.py
+++ d18/solve.py
@@ -1,7 +1,23 @@
+from utils import Vector
+LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+
 def solve(file):
     with open(file) as f:
-        lines = f.readlines()
+        mace = f.readlines()
 
+    walls = set()
+    keys = dict()
+    doors = dict()
+
+    for x, row in enumerate(mace):
+        for y, cell in enumerate(row):
+            if cell == '#':
+                walls.add(Vector(x,y))
+
+            if cell in LETTERS.lower():
+                keys[Vector(x,y)] = cell
+            if cell in LETTERS.upper():
+                doors[Vector(x,y)] = cell
 
 if __name__ == '__main__':
     solve('input_test_1.txt')
